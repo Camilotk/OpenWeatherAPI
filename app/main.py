e7fcfce536df75e5091a64204f470efa176deef6
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-04-09 03:09:07 -0300

    :recycle: changed the structure of the method to suport caching

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 import os
+from unicodedata import name
 import requests
 
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
+from .models import Weather
+from db.database import Cache
 
 env = load_dotenv()
 app = Flask(__name__)
@@ -18,15 +21,22 @@ def get_temperature(city_name):
     url_temperature_api = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}'
     data = requests.get(url_temperature_api).json()
 
-    return_data = {
-        'min': data['main']['temp_min'],
-        'max': data['main']['temp_max'],
-        'avg': float(format((data['main']['temp_min'] + data['main']['temp_max'])/2, '.2f')),
-        'city': {
-            'name': data['name'],
-            'country': data['sys']['country']
-        }
+    avg = float(format((data['main']['temp_min'] + data['main']['temp_max'])/2, '.2f'))
+
+    data = {
+        'name': data['name'], 
+        'country': data['sys']['country'],  
+        'min': data['main']['temp_min'], 
+        'max': data['main']['temp_max'], 
+        'avg': avg
     }
 
-    return return_data
+    parameter = request.args.get('max', default = 1, type = int)
 
+    if parameter:
+        return {'p': parameter}
+
+    weather = Weather(**data)
+    cache = Cache()
+
+    return weather.dict()
