36a7edda8f506d1f30dac6d35794946e1031d41d
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-04-08 21:00:53 -0300

    :hammer: Add the first endpoint to API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
-from flask import Flask
+import os
+import requests
 
+from flask import Flask
+from dotenv import load_dotenv
+
+env = load_dotenv()
 app = Flask(__name__)
+
+API_KEY = os.getenv('API_KEY')
 
 @app.get('/temperature/<city_name>')
 def get_temperature(city_name):
-    return f"<h1>{city_name}</h1>"
+    url_geocoding_api = f'http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=5&appid={API_KEY}'
+    place = requests.get(url_geocoding_api).json()
+    lat, lon = (place[0]['lat'], place[0]['lon'])
+
+    url_temperature_api = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}'
+    data = requests.get(url_temperature_api).json()
+
+    return data
