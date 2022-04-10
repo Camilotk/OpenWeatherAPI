3652eb446d561b7840b46263b7df725f90ba0553
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-04-10 19:51:47 -0300

    :white_check_mark: add test for cache time.

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,8 @@ import requests
 
 from dotenv import load_dotenv
 from db.database import Cache
-from app.main import return_if_cache_is_valid, calc_average_temperature
+from app.main import return_if_cache_is_valid, cache_is_valid, calc_average_temperature
+from datetime import datetime, timedelta
 
 env = load_dotenv()
 BASE_URL = os.getenv('BASE_URL')
@@ -32,7 +33,14 @@ def test_get_temperature(city_name):
     response = requests.get(url)
     assert response.status_code == 200
 
-def test_cache_is_valid(cache_database):
+@pytest.mark.parametrize("cache_time, expected", [
+    ((datetime.now() - timedelta(1)), False),
+    (datetime.today(), True)
+])
+def test_cache_is_valid(cache_time, expected):
+    assert cache_is_valid(cache_time) == expected
+
+def test_return_cache_is_valid(cache_database):
     cache_database.create_cache_entry('Bento Gonçalves', 'Brasil', 35.7, 22.3)
     retrieved_cache = cache_database.get_cached_temperatures('Bento Gonçalves', 1)
     response = return_if_cache_is_valid(retrieved_cache)
